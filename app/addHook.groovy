12597ff6b1166b85ede2251b8d153a58da37a723
Kunal Dabir <kunal.dabir@gmail.com>
2014-11-12 20:37:17 +0000

    got it working locally

--- app/addHook.groovy
+++ app/addHook.groovy
@@ -2,4 +2,6 @@ def hook = new Hook(params.subMap(["sent_to", "post_to", "payload"]))
 //hook.user = user
 hook.save()
 
+log.info "$hook saved"
+
 println "hook created"
--- app/receiveEmail.groovy
+++ app/receiveEmail.groovy
@@ -2,4 +2,4 @@ def msg = mail.parseMessage(request)
 
 log.info "Subject ${msg.subject}, to ${msg.allRecipients.join(', ')}, from ${msg.from[0]}"
 
-forward "/post.groovy?sent_to=${params.sent_to}"
+forward "/post.groovy?"
