fe72643417b237ae892072cb93dc17ce5b0578ac
Kunal Dabir <kunal.dabir@gmail.com>
2014-11-13 09:33:14 +0530

    log the response data

--- app/post.groovy
+++ app/post.groovy
@@ -2,3 +2,4 @@ def hook = Hook.get(params.sent_to)
 
 def resp = new URL(hook.post_to).post(payload:hook.payload.bytes)
 
+log.info("got response from ${hook.post_to} [${resp.statusCode}] ${resp.headers}")
